59126ddbf9dfdcfb2bd4748453afd4ebfde2759e
NUshakiran <ushakirannarena@gmail.com>
2024-01-26 17:35:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './components/nav';
+import Body from './components/body';
+import CreateUser from './components/createuser';
+import OTP from './components/otp';
+import Sucess from './components/successful';
+import AllPost from './components/allPost';
+import CreatePost from './components/createpost'; 
+import GetData from './components/getdata';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/login" element={<Body />} />
+        <Route path="/createuser" element={<CreateUser />} />
+        <Route path="/otp" element={<OTP />} />
+        <Route path="/sucess" element={<Sucess />} />
+        <Route path="/post/" element={<AllPost />} >
+        <Route path="createuser" element={<CreatePost />} />
+        <Route path="getpostdata" element={<GetData />} />
+        <Route path="getcommentdata" element={<GetData />} />
+        <Route path="getreplieddata" element={<GetData />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
